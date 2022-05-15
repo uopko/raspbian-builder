a0aef53fd84032a7bb6e1d6d381b3b984af38074
sam <88991878+uopko@users.noreply.github.com>
2022-05-15 09:29:29 +0800

    Delete builder directory

--- builder/stage2/00-copies-and-fills/02-run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-mv docker_volume.tar.gz ${ROOTFS_DIR}/docker_volume.tar.gz
-if [ -f "${ROOTFS_DIR}/etc/ld.so.preload" ]; then
-   mv "${ROOTFS_DIR}/etc/ld.so.preload" "${ROOTFS_DIR}/etc/ld.so.preload.disabled"
-fi
-
